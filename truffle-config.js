e719f7438717cd51f8dd8b737107f59c438dca25
Julio Martínez <jcheca@freepik.com>
2022-05-17 16:08:28 +0200

    set ganache as generic environment

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,12 @@
 const { projectId, mnemonic } = require('./secrets.json');
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+//const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
     networks: {
         development: {
-            host: "127.0.0.1",
+            host: "ganache",
             port: 7545,
             network_id: "*" // Match any network id
         },
